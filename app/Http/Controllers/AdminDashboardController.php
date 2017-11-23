16f0af0899d9f5600f76340e593d72abe5d60304
jkluio132465 <jkluio132465@gmail.com>
2017-11-23 23:10:43 +0800

    用model查詢資料

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 use App\Http\Requests;
 
 class AdminDashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard.index');
+    	$posts = Post::orderBy('created_at','DESC')->get();
+        $data =['posts'=>$posts];
+        return view('admin.dashboard.index',$data);
     }
 }
--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
+use App\Post;
 class AdminPostsController extends Controller
 {
     public function index()
-    {
+    {   
+        
         return view('admin.posts.index');
     }
 
