661d87a8260a255536d41dee6fd5982f5f18f976
jkluio132465 <jkluio132465@gmail.com>
2017-11-24 02:23:04 +0800

    將錯誤訊息製成共同區塊並include

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -16,20 +16,10 @@
         </ol>
     </div>
 </div>
-<div class="row">
-    <div class="col-lg-12">
-        <div class="alert alert-danger alert-dismissable">@if($errors->any())
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
-            @foreach($errors->all() as $error)
-                 <li>{{$error}}</li>
-             @endforeach
-        @endif
-         </div>
-</div>       
 
 <!-- /.row -->
->
+@include('admin.layouts.partials.validation')
+
 <!-- /.row -->
 
 <div class="row">
