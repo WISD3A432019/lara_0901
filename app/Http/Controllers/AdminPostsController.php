1d5394cbb6c4b8fba8b13f251bc0a89aa99d83e9
jkluio132465 <jkluio132465@gmail.com>
2017-11-24 01:19:25 +0800

    刪除資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -38,5 +38,10 @@ class AdminPostsController extends Controller
         $post->update($request->all());
         return redirect()->route('admin.posts.index');
     }
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        return redirect()->route('admin.posts.index');
+    }
 
 }
