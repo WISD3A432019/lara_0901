b34c71e863a2ddfcfe86f9dfc0c86d4c1912b3d2
jkluio132465 <jkluio132465@gmail.com>
2017-11-24 01:46:15 +0800

    錯誤訊息index更正create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -16,16 +16,20 @@
         </ol>
     </div>
 </div>
-<!-- /.row -->
-
 <div class="row">
     <div class="col-lg-12">
-        <div class="alert alert-danger alert-dismissable">
+        <div class="alert alert-danger alert-dismissable">@if($errors->any())
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
-        </div>
-    </div>
-</div>
+            @foreach($errors->all() as $error)
+                 <li>{{$error}}</li>
+             @endforeach
+        @endif
+         </div>
+</div>       
+
+<!-- /.row -->
+>
 <!-- /.row -->
 
 <div class="row">
